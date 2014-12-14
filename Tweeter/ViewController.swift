8080d248c542a0935d4c119bd5890f652c7f2d32
Thomas England <tengland808@gmail.com>
2014-12-13 18:07:25 -1000

    Initial Commit

---
+++ Tweeter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tweeter
+//
+//  Created by THOMAS ENGLAND on 12/13/14.
+//  Copyright (c) 2014 funcTom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
